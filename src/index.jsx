24a26fbabfeabe2eb7728fdee02ec56b3850ca83
= <neymegen50@gmail.com>
2022-07-19 15:46:47 +0300

    message

--- src/index.jsx
+++ src/index.jsx
@@ -7,5 +7,5 @@ const rootElement = document.querySelector('#root');
 ReactDOM.render(<Greeting 
   firstName="John" 
   lastName ="Doe" 
-  birthDate={Math.floor((new Date() - new Date('2001-01-01T11:11:11.819Z')) / 1000 / 60 / 60 / 24 / 365.25)}/>, 
+  birthDate={Math.floor((new Date('2019-01-01T11:11:11.819Z') - new Date('2001-01-01T11:11:11.819Z')) / 1000 / 60 / 60 / 24 / 365.25)}/>, 
   rootElement);
