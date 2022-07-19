ee2942b66371bb4e11c1ed1e4cb8c88dc122bf10
= <neymegen50@gmail.com>
2022-07-19 15:39:12 +0300

    init files

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import Greeting from './Greeting.jsx';
+
+const rootElement = document.querySelector('#root');
+ReactDOM.render(<Greeting 
+  firstName="John" 
+  lastName ="Doe" 
+  birthDate={Math.floor((new Date() - new Date('2001-01-01T11:11:11.819Z')) / 1000 / 60 / 60 / 24 / 365.25)}/>, 
+  rootElement);
