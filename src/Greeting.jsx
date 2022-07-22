810fa8b1c476f716405da8e9c9332cd6cd9e2619
= <neymegen50@gmail.com>
2022-07-22 11:53:26 +0300

    another message

--- src/Greeting.jsx
+++ src/Greeting.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './index.scss';
 import moment from 'moment';
 
-const formatDate = date => moment(date).format("DD MMM YYYY");
+export const formatDate = date => moment().diff(moment(date, "DD MMM YYYY"), 'years');
+
+// moment(date).format("DD MMM YYYY");
 
 const Greeting = (props) => {
   return (
     <div className="greeting">
-    {`My name is ${props.firstName} ${props.lastName}. I'm ${formatDate(birthDate)} years old`}
+    {`My name is ${props.firstName} ${props.lastName}. I'm ${formatDate(props.birthDate)} years old`}
     </div>
 );
 };
--- src/index.jsx
+++ src/index.jsx
@@ -9,5 +9,5 @@ const rootElement = document.querySelector('#root');
 ReactDOM.render(<Greeting 
   firstName="John" 
   lastName ="Doe"  
-  birthDate = {new Date('2018-01-01T11:32:19.566Z') - new Date('2001-01-01T11:32:19.566Z')}/>,
+  birthDate = {new Date('2005-01-01T11:32:19.566Z')}/>,
   rootElement);
